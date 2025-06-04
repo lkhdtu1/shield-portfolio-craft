db9397311ff9cd30e9f6eeeebf0ebf0ecae94521
TAMIS1 <mohammedtamis0@gmail.com>
2025-06-04 11:49:16 +0100

    hosting changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -21,5 +20,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/shield-portfolio-craft/'  // Must match your repository name
 }));
